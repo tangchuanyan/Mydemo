74baf082fe970173b2adc7d4bce5992331824154
tangchuanyan <tangchuanyan@cmhi.chinamobile.com>
2019-07-30 21:54:00 +0800

    First Commit

---
+++ src/Mydemo1.java
@@ -0,0 +1,10 @@
+public class Mydemo1 {
+    public void add(){
+        System.out.println("add............");
+    }
+    public static void main(String[] args){
+        Mydemo1 mydemo=new Mydemo1();
+        mydemo.add();
+
+    }
+}
